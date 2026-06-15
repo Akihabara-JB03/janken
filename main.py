132219849969e21cf80d36e5326673051f388942
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-16 04:59:35 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
-aiko,kati,make = 0,0,0
+aiko,kati,make,siai_kaisu,syouritu = 0,0,0,0,0
 def janken(user_hand):
-    global aiko,make,kati
+    global aiko,make,kati,siai_kaisu,syouritu
     ai_te = random.randint(0,2)
-    
+    siai_kaisu+=1
     if user_hand == ai_te:
         print("次が勝負ですね！！")
         aiko+=1
@@ -14,3 +14,13 @@ def janken(user_hand):
         print("僕の勝ちだ！！ふぇえ～～い！！")
         make+=1
     print(f"勝ち:{kati}回　負け:{make}回 あいこ:{aiko}回")
+    syouritu = kati / siai_kaisu
+    print(f"あなたの勝率は、{syouritu*100}%です")
+    if syouritu*100 > 95:
+        print("いいな～！羨ましいな～")
+    elif syouritu*100 <= 95 and syouritu*100 > 74:
+        print("すごいね～。")
+    else:
+        print("運が悪いんですね～")
+    
+    print("これでも、130~140KiBを維持しているんですよ。流石C言語！！")
