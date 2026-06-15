8dee540600728ebe65c9a0373d4d6f0540276922
Akihabara-JB03 <akabanestation1007@gmail.com>
2026-06-16 04:35:29 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import random
-
+aiko,kati,make = 0,0,0
 def janken(user_hand):
+    global aiko,make,kati
     ai_te = random.randint(0,2)
+    
     if user_hand == ai_te:
         print("次が勝負ですね！！")
+        aiko+=1
     elif (user_hand - ai_te + 3) % 3 == 2:
         print("僕の負けか～！！次だ次！！")
+        kati+=1
     else:
         print("僕の勝ちだ！！ふぇえ～～い！！")
+        make+=1
+    print(f"勝ち:{kati}回　負け:{make}回 あいこ:{aiko}回")
